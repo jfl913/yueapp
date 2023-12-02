5a51397b3ff484a308384524bd116601cf6851a6
李俊峰 <jfl913@163.com>
2023-12-02 20:11:49 +0800

    shein_images_downloader 网页开发完成

--- docs/components/SheinImagesDownloaderPricing.vue
+++ docs/components/SheinImagesDownloaderPricing.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-const freePlanFeatures = ['Download images 3 times per day', 'Get the best image quality', 'Save all images in one click (main, variant, description)', 'Save images as .ZIP file format']
-const proPlanFeatures = ['Download main images unlimited', 'Download variants images unlimited', 'Download description images unlimited', 'Get the best image quality', 'Save all images in one click (main, variant, description)', 'Save images as .ZIP file format']
+const freePlanFeatures = ['Download product images 3 times per day', 'Get the best image quality', 'Save all images in one click', 'Save images as .ZIP file format']
+const proPlanFeatures = ['Download product images unlimited', 'Get the best image quality', 'Save all images in one click', 'Save images as .ZIP file format']
 
 </script>
 
@@ -119,26 +119,6 @@ const proPlanFeatures = ['Download main images unlimited', 'Download variants im
                                     </svg>
                                     <span>{{ proPlanFeatures[3] }}</span>
                                 </li>
-                                <li class="flex items-center space-x-3">
-                                    <!-- Icon -->
-                                    <svg class="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400" fill="currentColor"
-                                         viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                        <path fill-rule="evenodd"
-                                              d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                                              clip-rule="evenodd"></path>
-                                    </svg>
-                                    <span>{{ proPlanFeatures[4] }}</span>
-                                </li>
-                                <li class="flex items-center space-x-3">
-                                    <!-- Icon -->
-                                    <svg class="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400" fill="currentColor"
-                                         viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                        <path fill-rule="evenodd"
-                                              d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                                              clip-rule="evenodd"></path>
-                                    </svg>
-                                    <span>{{ proPlanFeatures[5] }}</span>
-                                </li>
                         </ul>
                     </div>
                     <!-- Pricing Card -->
@@ -146,7 +126,7 @@ const proPlanFeatures = ['Download main images unlimited', 'Download variants im
                          class="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow dark:border-gray-600 xl:p-8 dark:bg-gray-800 dark:text-white">
                         <h3 class="mb-4 text-2xl font-semibold">Quarterly Pro</h3>
                         <div class="flex justify-center items-baseline my-8">
-                            <span class="mr-2 text-5xl font-extrabold">$17.99</span>
+                            <span class="mr-2 text-5xl font-extrabold">$24.99</span>
                             <span class="text-gray-500 dark:text-gray-400" dark:text-gray-400>/quarter</span>
                         </div>
                         <!-- List -->
@@ -190,26 +170,6 @@ const proPlanFeatures = ['Download main images unlimited', 'Download variants im
                                               clip-rule="evenodd"></path>
                                     </svg>
                                     <span>{{ proPlanFeatures[3] }}</span>
-                                </li>
-                                <li class="flex items-center space-x-3">
-                                    <!-- Icon -->
-                                    <svg class="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400" fill="currentColor"
-                                         viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                        <path fill-rule="evenodd"
-                                              d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                                              clip-rule="evenodd"></path>
-                                    </svg>
-                                    <span>{{ proPlanFeatures[4] }}</span>
-                                </li>
-                                <li class="flex items-center space-x-3">
-                                    <!-- Icon -->
-                                    <svg class="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400" fill="currentColor"
-                                         viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                        <path fill-rule="evenodd"
-                                              d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                                              clip-rule="evenodd"></path>
-                                    </svg>
-                                    <span>{{ proPlanFeatures[5] }}</span>
                                 </li>
                         </ul>
                     </div>
@@ -262,26 +222,6 @@ const proPlanFeatures = ['Download main images unlimited', 'Download variants im
                                           clip-rule="evenodd"></path>
                                 </svg>
                                 <span>{{ proPlanFeatures[3] }}</span>
-                            </li>
-                            <li class="flex items-center space-x-3">
-                                <!-- Icon -->
-                                <svg class="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400" fill="currentColor"
-                                     viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                    <path fill-rule="evenodd"
-                                          d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                                          clip-rule="evenodd"></path>
-                                </svg>
-                                <span>{{ proPlanFeatures[4] }}</span>
-                            </li>
-                            <li class="flex items-center space-x-3">
-                                <!-- Icon -->
-                                <svg class="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400" fill="currentColor"
-                                     viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                    <path fill-rule="evenodd"
-                                          d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                                          clip-rule="evenodd"></path>
-                                </svg>
-                                <span>{{ proPlanFeatures[5] }}</span>
                             </li>
                         </ul>
                     </div>
---
+++ docs/shein-images-downloader.md
@@ -0,0 +1,34 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "Shein Images Downloader"
+  text: "Download hi-res images for shein in one click"
+  tagline: A handy tool that allows you to easily download images from the shein website. 
+  image: /shein-images-downloader.png
+  actions:
+    - theme: brand
+      text: Add to Chrome
+      link: https://chrome.google.com/webstore/detail/ig-auto-follow/iiaohnpoogjkomcdkhdfljgpglejpaad?hl=zh-CN&authuser=0
+
+features:
+  - title: Feature One
+    details: Download product images.
+  - title: Feature Two
+    details: Best quality image.
+  - title: Feature Three
+    details: Save all images in one click.
+  - title: Feature Four
+    details: Save images as .ZIP file format.
+---
+
+<script setup>
+    import SheinImagesDownloaderPricing from './components/SheinImagesDownloaderPricing.vue'
+    import SheinImagesDownloaderFAQ from './components/SheinImagesDownloaderFAQ.vue'
+    import Checkout from './Checkout.vue'
+</script>
+
+<SheinImagesDownloaderPricing />
+<SheinImagesDownloaderFAQ />
+<Checkout chrome-extension-name="shein_images_downloader" />
