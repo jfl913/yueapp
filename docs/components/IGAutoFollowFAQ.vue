99a6dc10d5450712a9d7ba8da78386d879fdc81b
李俊峰 <jfl913@163.com>
2023-12-21 17:16:51 +0800

    How to find a post’s link?

--- docs/components/IGAutoFollowFAQ.vue
+++ docs/components/IGAutoFollowFAQ.vue
@@ -57,6 +57,29 @@ import { ref } from 'vue'
                                 users per day (6 users per hour). There is a delay of 10 minutes between each follow or
                                 unfollow to keep your account safe from Instagram’s detection.</p>
                         </div>
+                        <div class="mb-10">
+                            <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                                <svg class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor"
+                                     viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd"
+                                          d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+                                          clip-rule="evenodd"></path>
+                                </svg>
+                                How to find a post’s link?
+                            </h3>
+                            <p class="text-gray-500 dark:text-gray-400">You will find a post’s link like this: <a
+                                   class="text-red-500 dark:text-red-400" href="https://www.instagram.com/p/C0-HhYZyT1O/"
+                                   target="_blank">https://www.instagram.com/p/C0-HhYZyT1O/</a> or this <a
+                                   class="text-red-500 dark:text-red-400" href="https://www.instagram.com/reel/Cz7kuRYva2P/"
+                                   target="_blank">https://www.instagram.com/reel/Cz7kuRYva2P/</a>.</p>
+                            <p class="text-gray-500 dark:text-gray-400">The extension will automatically enter the URL of
+                                the post when you click on it after opening the post.</p>
+                            <p class="text-gray-500 dark:text-gray-400">Or you can see how to find a post's link from
+                                the picture below.</p>
+                            <img src="https://qurgxyievnxstbfbqrnu.supabase.co/storage/v1/object/public/images/Xnip2023-12-21_17-03-21.jpg" alt="">
+                            <img src="https://qurgxyievnxstbfbqrnu.supabase.co/storage/v1/object/public/images/Xnip2023-12-21_17-04-13.jpg" alt="">
+                            <img src="https://qurgxyievnxstbfbqrnu.supabase.co/storage/v1/object/public/images/Xnip2023-12-21_17-05-32.jpg" alt="">
+                        </div>
                     </div>
                     <div>
                         <div class="mb-10">
@@ -100,13 +123,13 @@ import { ref } from 'vue'
                                 How to refund?
                             </h3>
                             <p class="text-gray-500 dark:text-gray-400">You can refund within 7 days, just send an email to
-                            <a href="mailto:jfl913@gmail.com">jfl913@gmail.com</a> and tell us your account email and
-                            receipt order id, You will receive your refund in the next few days.
-                        </p>
+                                <a href="mailto:jfl913@gmail.com">jfl913@gmail.com</a> and tell us your account email and
+                                receipt order id, You will receive your refund in the next few days.
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
     </section>
 </div></template>
 
